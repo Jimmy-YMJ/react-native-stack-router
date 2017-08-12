534676e2e0fd6d6ed3297e62bdc87598759d6cf3
Jimmy-YMJ <jimmy@symfx.com>
2017-08-12 20:49:31 +0800

    Export ComponentBase and PureComponentBase

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import StackRouter from './src/StackRouter';
 import StackAwareWrapper from './src/StackAwareWrapper';
+import ComponentBase from './src/ComponentBase';
+import PureComponentBase from './src/PureComponentBase';
 
 module.exports = {
   StackRouter: StackRouter,
-  StackAwareWrapper: StackAwareWrapper
+  StackAwareWrapper: StackAwareWrapper,
+  ComponentBase: ComponentBase,
+  PureComponentBase: PureComponentBase
 };
